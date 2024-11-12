75cbf876bf1c24eda50fa545e4bbc8d2b1cbf5b8
Destiny653 <fokundem.com@gmail.com>
2024-11-12 11:54:01 +0100

    potential2

--- src/app/components/Login/Login.jsx
+++ src/app/components/Login/Login.jsx
@@ -19,7 +19,7 @@ export default function Login() {
             password: password
         }
         try {
-            const response = await fetch('https://dashboard-m2bd.onrender.com/api/auth/login', {
+            const response = await fetch('https://dashboard-m2bd.onrender.com/api/admin/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
